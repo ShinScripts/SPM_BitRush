167ff3138b5783ee8d55c446d3fd5e10b45be999
MelvinLagerman <104003474+MelvinLagerman@users.noreply.github.com>
2023-06-01 19:25:08 +0200

    TurretUpdated

--- Source/Bit_Rush/EnemyLaserTurret.h
+++ Source/Bit_Rush/EnemyLaserTurret.h
@@ -50,12 +50,12 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess))
 	float RotationSpeed;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess))
-	float AttackRange = 2000;
-
+	
 	
 	UPROPERTY(VisibleDefaultsOnly, Category = "Rockets")
 	TSubclassOf<class AActor> LaserClass;
+
+	FHitResult LaserHit;
 
 	//Functions
 	
